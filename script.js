c08b202c242922d670ed9c5a98aac0bea1f74106
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-13 19:48:32 +0530

    Slider

---
+++ script.js
@@ -0,0 +1,9 @@
+const wrapper = document.querySelector(".sliderWrapper");
+
+const menuItems = document.querySelectorAll(".menuItem");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    wrapper.style.transform = `translateX(-${index}00vw)`;
+  });
+});
